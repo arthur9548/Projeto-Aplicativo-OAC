fb2f24cde531798613c3a1efe25571bd64357264
Arthur Botelho <tucobotelho@gmail.com>
2025-02-16 13:51:54 -0300

    adicionando exemplo de tile

--- test/teste_video.s
+++ test/teste_video.s
@@ -9,41 +9,37 @@ MAIN:
 call INIT_FRAMES
 li a0, 0x0ff
 GET_BUFFER_TO_DRAW(a7)
-call FILL_SCREEN_FROM
-li a0, mine_tile
-li a1, -6
-li a2, 236
-li a3, 12
-li a4, 12
-call DRAW_IMAGE
-li a0, enderman_transparent
-li a1, 316
-li a2, 236
-li a3, 12
-li a4, 12
-call DRAW_IMAGE
-li a0, mine_tile
-li a1, 316
-li a2, -6
-li a3, 12
-li a4, 12
-call DRAW_IMAGE
-li a0, enderman_transparent
-li a1, -6
-li a2, -6
-li a3, 12
-li a4, 12
-call DRAW_IMAGE
-call SWAP_FRAMES
-GET_BUFFER_TO_DRAW(a7)
-sleep(1000)
-li a0, 0x0ff
 li a1, 0
 call FILL_SCREEN_FROM
-li a0, 0x03
-li a1, 160
-call FILL_SCREEN_FROM
+li a0, mine_tile
+li a1, -8
+li a2, 232
+call DRAW_TILE
+li a0, enderman_transparent
+li a1, 316
+li a2, 236
+li a3, 12
+li a4, 12
+call DRAW_IMAGE
+li a0, mine_tile
+li a1, 312
+li a2, -8
+call DRAW_TILE
+li a0, mine_tile
+li a1, 16
+li a2, 16
+call DRAW_TILE
 call SWAP_FRAMES
 exit_loop
+
+TRY_MENU: #a1 = tamanho de teste, a7 = tela
+	li a0, 0x0ff
+	memo(a1)
+	li a1, 0
+	call FILL_SCREEN_FROM
+	li a0, 0x03
+	unmemo(a1)
+	call FILL_SCREEN_FROM
+	call SWAP_FRAMES
 
 .include "../src/video/video.s"
