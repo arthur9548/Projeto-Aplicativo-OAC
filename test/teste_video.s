63573d8cc732443e4287dca5ec22c90a5f6d1501
Arthur Botelho <tucobotelho@gmail.com>
2025-02-17 12:10:03 -0300

    renderização funcionando

--- test/teste_video.s
+++ test/teste_video.s
@@ -1,11 +1,44 @@
 # teste da renderização visual
 
-.include "../src/data/data.s"
 .include "../src/util/util.s"
+.include "../src/data/data.s"
 
 .text
 
 MAIN:
+la s0, GAME_STATE
+li t0, GAME_STATE_INIT
+sb t0, 0(s0)
+call GAME_RENDER
+li t0, GAME_STATE_ACTION
+sb t0, 0(s0)
+la t0, mapa_de_testes
+la t1, MAP_ADDRESS
+sw t0, 0(t1)
+ACTION:
+la t0, MAP_OFFSET
+lh t0, 0(t0)
+li t1, 100
+beq t0, t1, END
+addi t0, t0, 1
+la t1, MAP_OFFSET
+sh t0, 0(t1)
+la t1, PLAYER_X
+lh t0, 0(t1)
+addi t0, t0, 2
+sh t0, 0(t1)
+call GAME_RENDER
+sleep(10)
+j ACTION
+END:
+sleep(1000)
+li t0, GAME_STATE_OVER
+sb t0, 0(s0)
+call GAME_RENDER
+exit_loop
+
+
+SLIDE_TEST:
 call INIT_FRAMES
 li s0, 0 #offset inicial
 la s1, mapa_de_testes
@@ -24,7 +57,7 @@ LOOP:
 	li a1, 160
 	call FILL_SCREEN_FROM
 	call SWAP_FRAMES
-	sleep(50)
+	#sleep(50)
 	addi s0, s0, 1
 	j LOOP
 END:
