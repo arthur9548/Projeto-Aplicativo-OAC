1d6d3f403be129b50f3ecf0baffc779dca717076
Arthur Botelho <tucobotelho@gmail.com>
2025-02-16 21:15:59 -0300

    adicionando teste de áudio

--- test/teste_audio.s
+++ test/teste_audio.s
@@ -1,1 +1,18 @@
 # teste de audio
+.data
+.include "../src/data/data.s"
+.include "../src/data/test_data.s"
+.include "../src/util/util.s"
+
+.text
+
+MAIN:
+	la a0, START_MENU_SONG
+	li a1, 1
+	li a2, 100
+	call playAudio
+	lw t0, 4(a0)
+	beq t0, zero, END #não toca do início
+	j MAIN
+END: exit_system
+.include "../src/audio/audio.s"
