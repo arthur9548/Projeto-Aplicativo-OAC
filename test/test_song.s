8071d691bf11774323a2927613e5fd330bef744d
arquiago <85774510+arquiago@users.noreply.github.com>
2025-02-18 19:52:07 -0300

    teste com novas músicas

--- test/test_song.s
+++ test/test_song.s
@@ -3,22 +3,24 @@
 .include "../MACROSv24.s"
 
 .text
-	#la a0, START_SONG
-	#call PLAY_WHOLE_SONG
+	la a0, START_SONG
+	call PLAY_WHOLE_SONG
 
 	#la a0, PULA
 	#call PLAY_WHOLE_SONG
-	#sleep(500)
 
 	#la a0, ASPIRA
 	#call PLAY_WHOLE_SONG
-	#sleep(500)
 
-	la a0, PULA
-        call PLAY_WHOLE_SONG
-        
-        sleep(500)
+	la a0, START_SONG
+	call PLAY_WHOLE_SONG
 
+	la a0, DIE_SONG
+	call PLAY_WHOLE_SONG
+
+	la a0, FASE_SONG
+	
+	call PLAY_WHOLE_SONG
 	li a7, 10
 	ecall
 .include "../src/audio/audio.s"
