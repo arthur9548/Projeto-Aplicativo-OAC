f0861a149b698a9a0b1f1fee49e36286d6f57154
arquiago <85774510+arquiago@users.noreply.github.com>
2025-02-18 17:49:05 -0300

    testa sons novos

--- test/test_song.s
+++ test/test_song.s
@@ -3,8 +3,15 @@
 .include "../MACROSv24.s"
 
 .text
-	la a0, Fingerbib_Track1_Voice1_SIZE
+	la a0, START_SONG
 	call PLAY_WHOLE_SONG
+
+	la a0, PULA
+	call PLAY_WHOLE_SONG
+
+	la a0, ASPIRA
+	call PLAY_WHOLE_SONG
+
 	li a7, 10
 	ecall
 .include "../src/audio/audio.s"
