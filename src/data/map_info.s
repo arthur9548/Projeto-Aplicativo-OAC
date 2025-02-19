a8c01023e02a018bbf9ea9988cde1b3717b9c44a
Arthur Botelho <tucobotelho@gmail.com>
2025-02-18 23:53:35 -0300

    desenvolvendo inimigos

--- src/data/map_info.s
+++ src/data/map_info.s
@@ -3,5 +3,8 @@
 MAP_OFFSET: .half 0
 MAP_ADDRESS: .word 0
 MAP_TILE_SPRITES: .word 0, 0, 0, 0, 0, 0 #fundo, bloco, proibido, porta aberta, porta fechada, reserva
-ENEMIES_ADDRESS: .word 0
+#inimigos são caracterizados por:
+#posição (word), velocidade (word), tipo (byte), vida (byte), direção (byte), ativo(byte), contador (word), extra(word)
+#máximo de inimigos ativos ao mesmo tempo: 50
+ENEMIES_ADDRESS: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 SHOTS_ADDRESS: .word 0
